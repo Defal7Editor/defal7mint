30242f11f7c0f5d74a0ad919744329729a99a411
Defal7Editor <32604521+Defal7Editor@users.noreply.github.com>
2020-08-11 19:19:21 -0300

    Add files via upload

--- getstuff.sh
+++ getstuff.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sudo apt install bzip2 openssh-server samba steam wine
+sudo apt install bzip2 openssh-server samba steam wine openjdk-14-jdk p7Zip-full
+sudo dpkg -s p7Zip-full | grep Status
