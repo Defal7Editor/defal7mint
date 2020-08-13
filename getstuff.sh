5f28d822f36f07e9df27eed3f947c773633d1d9e
Defal7 <32604521+Defal7c@users.noreply.github.com>
2020-08-13 19:48:09 -0300

    Add files via upload

--- getstuff.sh
+++ getstuff.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
 sudo add-apt-repository ppa:obsproject/obs-studio
-sudo apt install bzip2 openssh-server samba steam wine openjdk-14-jdk p7Zip-full ffmpeg obs-studio
-sudo dpkg -s p7Zip-full | grep Status
+sudo apt install openssh-server samba steam wine openjdk-14-jdk ffmpeg obs-studio git rabbitvcs-cli flameshot default-jdk
+sudo apt update
+sudo apt full-upgrade
