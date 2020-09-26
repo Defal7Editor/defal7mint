a18cf58b81b8383b3896195885d0d70a98c14256
Defal7 <mth.fe.a@protonmail.com>
2020-09-26 15:46:53 -0300

    Create setup_arch.sh

---
+++ setup_arch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo pacman -S openssh-server filezilla samba steam wine bleachbit openjdk-14-jdk ffmpeg git rabbitvcs-cli flameshot default-jdk lutris glances screen gimp github-cli
+sudo pacman -Syu
+
+
