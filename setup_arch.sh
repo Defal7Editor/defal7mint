551312f07e30be0a722db1083f59dc23a6182f68
Defal7 <mth.fe.a@protonmail.com>
2020-09-30 16:37:16 -0300

    Added keepassxc

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -Syu
-sudo pacman -S openssh filezilla samba steam discord wine bleachbit ffmpeg rabbitvcs-cli flameshot lutris glances screen gimp github-cli neofetch
+sudo pacman -S openssh filezilla samba steam discord wine bleachbit ffmpeg rabbitvcs-cli flameshot lutris glances screen gimp github-cli neofetch keepassxc
 
 # open ssh config
 
