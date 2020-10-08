abd332af03da9be6a1bb79d40e6b83acd263073f
Defal7 <mth.fe.a@protonmail.com>
2020-10-08 11:54:47 -0300

    yay update

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 sudo pacman -Syu
-sudo pacman -S openssh filezilla samba steam discord wine bleachbit ffmpeg rabbitvcs-cli flameshot lutris glances screen gimp github-cli neofetch keepassxc
+sudo pacman -S openssh filezilla samba base-devel steam discord wine bleachbit ffmpeg rabbitvcs-cli flameshot lutris glances screen gimp github-cli neofetch keepassxc
+
+#yay
+cd $HOME/Documents
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -s
+sudo pacman -U *
+
+#AUR stuff
+yay -S github-desktop-bin minecraft visual-studio-code-bin debtap
 
 # open ssh config
 
