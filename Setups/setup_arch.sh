05d25bafb184b3cedb205406242aec3aaf2d9f9c
Defal7 <mth.fe.a@protonmail.com>
2021-01-06 18:00:49 -0300

    Update setup_arch.sh

--- Setups/setup_arch.sh
+++ Setups/setup_arch.sh
@@ -28,13 +28,19 @@ pacman -Syu
 echo "---------------------------"
 echo "    Installing Packages    "
 echo "---------------------------"
-pacman -S openssh filezilla samba base-devel discord bleachbit ffmpeg flameshot lutris glances screen gimp github-cli neofetch keepassxc file-roller grub grub-customizer kdenlive obs-studio flatpak figlet
+pacman -S openssh filezilla samba base-devel discord bleachbit ffmpeg flameshot lutris glances screen gimp github-cli neofetch keepassxc file-roller grub grub-customizer kdenlive obs-studio flatpak figlet libreoffice vim
 
-echo "---------------------------"
-echo "    Installing Flatpaks    "
-echo "---------------------------"
+echo "----------------------------"
+echo " Installing Important Stuff "
+echo "----------------------------"
+pacman -S atom kate file-roller virt-manager gnome-calculator gnome-disk-utility gnome-multi-writer
+
+
+#echo "---------------------------"
+#echo "    Installing Flatpaks    "
+#echo "---------------------------"
 #flatpak
-flatpak install unityhub libreoffice
+#flatpak install unityhub libreoffice
 
 DOAS="Y"
 AWN=1
@@ -77,18 +83,18 @@ make install
 #yay
 cd $HOME
 git clone https://aur.archlinux.org/yay.git
-cd $HOME/yay
-makepkg -s
-sudo pacman -U *
+#cd $HOME/yay
+#makepkg -s
+#sudo pacman -U *
 
 #AUR stuff
-yay -S github-desktop-bin minecraft visual-studio-code-bin debtap auto-cpufreq rabbitvcs ttf-ms-fonts
+#yay -S github-desktop-bin minecraft visual-studio-code-bin debtap auto-cpufreq rabbitvcs ttf-ms-fonts
 
 #Debtap
-debtap -u
+#debtap -u
 
 #auto-cpufreq
-auto-cpufreq --install
+#auto-cpufreq --install
 
 # open ssh config
 systemctl enable sshd
