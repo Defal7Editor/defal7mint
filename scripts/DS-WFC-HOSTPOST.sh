de2b9958230e4580b6276c5fb974b384c9f95988
Defal <mth.fe.a@protonmail.com>
2021-04-17 12:15:05 -0300

    Added DS-WFC-HOSTPOST.sh

---
+++ scripts/DS-WFC-HOSTPOST.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+nmcli connection up NintendoWFC 
+https://gbatemp.net/threads/create-an-nds-compatible-hotspot-on-linux.543283/
--- scrpits/hackin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$EUID" != 0 ]
-then
-	echo Please run this script as Super User.
-	exit
-fi
-
-apt install wireshark
