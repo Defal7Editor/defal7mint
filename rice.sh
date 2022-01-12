e683590c7991448541e5b2efa63c3fa34c3521cf
defal7c <mth.fe.a@protonmail.com>
2022-01-12 14:01:01 -0300

    Updated rice.sh
    now it actually works

--- rice.sh
+++ rice.sh
@@ -4,45 +4,75 @@
 # 04/01/21
 # by defal
 
+SUDO=sudo
+#SUDO=doas
+
+BG=Wallpapers/Journey.webp
+
+Clone() {
+if [ -d RICE/$1 ]
+then
+	echo "[!] $1 already exists, skipping."
+	echo "[!] Try running ./rice.sh clean to remove it."
+else
+	if [ $2 = "git" ]
+	then
+		git clone https://github.com/Defal7c/$1 RICE/$1
+	elif [ $2 = "suckless" ]
+	then
+		wget https://dl.suckless.org/tools/$1
+	fi
+fi
+}
+
 if [[ $1 = clean ]]
 then
 	rm -rf RICE
     exit 0
 fi
 
-if [ "$EUID" -ne 0 ]
-then
-    echo "---------------------------"
-    echo "Please run this script as root."
-    echo "---------------------------"
-    exit 1
-fi
+#Clone my dwm
+Clone dwm git
 
-git clone https://github.com/Defal7c/dwm RICE/dwm
-git clone https://github.com/Defal7c/st RICE/st
+#Clone my st
+Clone st git
+
+# Dot-Files stuff
+Clone dot-files git
+
+cp RICE/dot-files/* ~/
+cp $BG ~/.bg
 
 cd RICE/
-wget https://dl.suckless.org/tools/dmenu-5.0.tar.gz RICE/
-wget https://dl.suckless.org/tools/scroll-0.1.tar.gz RICE/
-tar -xzvf dmenu-5.0.tar.gz
-tar -xzvf scroll-0.1.tar.gz
 
-#cleaning stuff
+DMENU=dmenu-5.0.tar.gz
+SCROLL=scroll-0.1.tar.gz
+Clone $DMENU suckless
+Clone $SCROLL suckless
+
+echo "[ Extracting dmenu ]"
+echo
+tar -xzvf dmenu-5.0.tar.gz
+echo "[ Extracting scroll ]"
+echo
+tar -xzvf scroll-0.1.tar.gz
+echo
+
+#Cleaning stuff
+Echo "[ Cleaning Stuff ]"
 rm scroll-0.1.tar.gz
 rm dmenu-5.0.tar.gz
 
-
 #dependencies
 cd scroll-0.1/
-echo "imagine echo make scroll here"
+$SUDO make clean install
 
 #stuff
 cd ../dmenu-5.0/
-echo "imagine echo make scroll here"
+$SUDO make clean install
 
 cd ../dwm/
-echo "imagine echo make scroll here"
+$SUDO make clean install
 
 cd ../st/
-echo "imagine echo make scroll here"
-#make clean install
+$SUDO make clean install
